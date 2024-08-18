28ba64e59a351583ce34749f35194aa70af8eb20
Sunny Kumar <sunny274.kr@gmail.com>
2024-08-18 13:23:20 +0530

    day 3

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Mobile from './Mobile'
+import Plusminus from './PlusMinus/Plusminus';
+
+function App() {
+  return (
+    <div className="App">
+      <section><Mobile/></section>
+      <section><Plusminus/></section>
+      <section>new</section>
+      <section>new1</section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PlusMinus/Plusminus.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Plusminus() {
+  return (
+    <div className='section'>Plusminus</div>
+  )
+}
