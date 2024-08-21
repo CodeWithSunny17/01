89956fb148db2a3797dc0c3b12e61984883796cf
Sunny Kumar <sunny274.kr@gmail.com>
2024-08-22 00:02:24 +0530

    day 4

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -55,37 +55,40 @@ export default function Calculator() {
     
   };
   return (
-    <div className="Cal">
-      <h1>{input}</h1>
-      <div className="1">
-        <button onClick={() => handleClick("C")}>C</button>
-        <button onClick={() => handleClick("<")}>&lt;</button>
-        <button onClick={() => handleClick("%")}>%</button>
-        <button onClick={() => handleClick("/")}>/</button>
-      </div>
-      <div className="2">
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("*")}>*</button>
-      </div>
-      <div className="3">
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("-")}>-</button>
-      </div>
-      <div className="4">
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("+")}>+</button>
-      </div>
-      <div className="5">
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick("00")}>00</button>
-        <button onClick={() => handleClick(".")}>.</button>
-        <button onClick={() => handleClick("=")}>=</button>
+    <div className="wrapper">
+      <div className="cal">
+
+        <h1>{input}</h1>
+        <div className="1">
+          <button onClick={() => handleClick("C")}>C</button>
+          <button onClick={() => handleClick("<")}>&lt;</button>
+          <button onClick={() => handleClick("%")}>%</button>
+          <button onClick={() => handleClick("/")}>/</button>
+        </div>
+        <div className="2">
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("*")}>*</button>
+        </div>
+        <div className="3">
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("-")}>-</button>
+        </div>
+        <div className="4">
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("+")}>+</button>
+        </div>
+        <div className="5">
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick("00")}>00</button>
+          <button onClick={() => handleClick(".")}>.</button>
+          <button onClick={() => handleClick("=")}>=</button>
+        </div>
       </div>
     </div>
   );
