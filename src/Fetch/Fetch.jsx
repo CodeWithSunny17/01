1ee9fa3ed55f6556964ddab2c9c8d8fd8cf165fd
Sunny Kumar <sunny274.kr@gmail.com>
2024-08-22 23:01:21 +0530

    day 5

--- src/Fetch/Fetch.jsx
+++ src/Fetch/Fetch.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Fetch() {
+
+  const[users, setUsers] = useState([])
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users').then(response => response.json()).then(data => setUsers(data))
+  })
   return (
-    <div>Fetch</div>
+    <div>
+      <h1>Users List</h1>
+      {users.map(u => {
+        return <li key={u.id}>
+            <a href={u.html_url}>{u.login}</a>
+          </li>
+      })}
+    </div>
   )
 }
