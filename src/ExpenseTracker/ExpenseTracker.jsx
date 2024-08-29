bb8325efb613409d63f7ac0615e0e2791d54d14f
Sunny Kumar <sunny274.kr@gmail.com>
2024-08-29 19:41:50 +0530

    day 6

--- src/ExpenseTracker/ExpenseTracker.jsx
+++ src/ExpenseTracker/ExpenseTracker.jsx
@@ -4,42 +4,37 @@ import { useState } from 'react';
 
 export default function ExpenseTracker() {
 
-    const [input, setInput] = useState({
-        expense: '',
-        amount: '',
-    })
-    const [display, setDisplay] = useState([]);
+    const [expense, setExpense] = useState('');
+    const [amount, setAmount] = useState('');
+    const [expenses, setExpenses] = useState([]);
 
-    const handleChange = (e) =>{
-        const {name, value} = e.target;
-        setInput((input)=>({
-            ...input,[name]: value
-        }))
-    }
 
 
     const handleClick = () =>{
+        if(!expense || !amount) return;
+        
         const newExpense = {
-            id: display.length+1,
-            expense: input.expense,
-            amount: input.amount,
+            id: expenses.length+1,
+            expense: expense,
+            amount: amount,
         }
-        setDisplay(()=>([...display, newExpense]))
-        setInput({})
+        setExpenses(([...expenses, newExpense]));
+        console.log(expenses)
+        setExpense('');
+        setAmount('');
+        
     }
 
     const handleDelete = (key)=> {
-        if(!input) return;
-
-        setDisplay(display.filter((display) => display.id !== key));
+        setExpenses(expenses.filter((expenses) => expenses.id !== key));
     }
 
   return (
     <div className='expense'>
         <h1>Expense Tracker</h1>
-        <input placeholder="Expense" name= "expense" type="text" onChange={handleChange} value={input.expense}/>
+        <input placeholder="Expense" name= "expense" type="text" onChange={(e)=>{setExpense(e.target.value)}} value={expense}/>
         <br />
-        <input placeholder="Amount" name= "amount" type="number" onChange={handleChange} value={input.amount}/>
+        <input placeholder="Amount" name= "amount" type="number" onChange={(e)=>{setAmount(e.target.value)}} value={amount}/>
         <br />
         <br />
         <button onClick={handleClick}>Add Expense</button>
@@ -47,11 +42,11 @@ export default function ExpenseTracker() {
         <br />
         <ul>
             {
-                display.map((display)=>{
-                    return <li key={display.id}>
-                            <div>{display.expense}  </div>
-                            <div>{display.amount}  </div>
-                            <button onClick={()=>handleDelete(display.id)}>Delete</button>
+                expenses.map((expenses)=>{
+                    return <li key={expenses.id}>
+                            <div>{expenses.expense}</div>
+                            <div>{expenses.amount}</div>
+                            <button onClick={()=>handleDelete(expenses.id)}>Delete</button>
                         </li>
                 })
             }
